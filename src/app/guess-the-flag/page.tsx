eb93800ac6e394b472f69798e251c29517cb6ad1
Deveesh Shetty <deveeshshetty@gmail.com>
2024-08-20 20:14:01 +0530

    feat: reveal solution for negative score

--- src/app/guess-the-flag/page.tsx
+++ src/app/guess-the-flag/page.tsx
@@ -2,10 +2,20 @@
 
 import Loading from "@/app/guess-the-flag/loading"
 import Search from "@/components/search"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { H1 } from "@/components/ui/typography"
 import { Country } from "@/lib/types"
 import { getRandomNumber } from "@/lib/utils"
-import { AlertCircle, Check, CheckCircle } from "lucide-react"
+import { AlertCircle, Check, CheckCircle, LightbulbIcon } from "lucide-react"
 import Image from "next/image"
 import { useEffect, useState } from "react"
 import { toast } from "sonner"
@@ -78,10 +88,40 @@ export default function GuessTheFlagPage() {
 
   return (
     <main className="px-4 py-12 md:p-12 container flex flex-col items-center justify-center mx-auto">
-      <div className="flex items-center justify-between w-full">
-        <p className="ml-auto px-8 font-bold  flex items-center gap-2 py-2 bg-primary text-white rounded-md">
+      <div className="flex items-center justify-end gap-2 w-full">
+        <p className="px-8 font-bold  flex items-center gap-2 py-2 bg-primary text-white rounded-md">
           Score: <span className="text-2xl">{score}</span>
         </p>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              variant="ghost"
+              className="rounded-full"
+              size="icon"
+              title="Take a hint"
+              onClick={() => {
+                setScore((prev) => prev - 20)
+                toast("-20 points for taking hint", {
+                  cancel: true,
+                  icon: <AlertCircle />,
+                  duration: 2000,
+                  style: { backgroundColor: "orange", color: "black" },
+                  position: "top-center",
+                })
+              }}
+            >
+              <LightbulbIcon fill="yellow" />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle className="text-center">The country is</DialogTitle>
+              <DialogDescription className="text-5xl text-primary p-8 text-center">
+                {country.name.common}.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
       <H1 className="mt-2 mb-8">Guess the Flag</H1>
       <div className="grid gap-2">
